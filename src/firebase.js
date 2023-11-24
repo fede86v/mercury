970c60e1e761c40b8bb6ddf97b89e3a7ad31ff1a
Federico Villa <fede86v@gmail.com>
2023-11-24 11:04:18 -0300

    Add Prod Credentials

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBkNQRCYF3EwkhSPNJqzFJS36KREvhrPro",
-  authDomain: "mercury-fe382.firebaseapp.com",
-  projectId: "mercury-fe382",
-  storageBucket: "mercury-fe382.appspot.com",
-  messagingSenderId: "225249121302",
-  appId: "1:225249121302:web:505d51ecd7599147881137"
+  apiKey: "AIzaSyC7b9COfOrS7hi1lLZpzjH8KAu0iF0ZbsI",
+  authDomain: "mercury-prod-cff3e.firebaseapp.com",
+  projectId: "mercury-prod-cff3e",
+  storageBucket: "mercury-prod-cff3e.appspot.com",
+  messagingSenderId: "166501381472",
+  appId: "1:166501381472:web:06942376c4fda1119c5a05"
 };
 
 // Initialize Firebase
@@ -16,4 +16,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { auth, db, app};
+export { auth, db, app };
