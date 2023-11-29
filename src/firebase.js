aaeaa497c44ff8bf565e7c89819faa73845d4a3d
Federico Villa <federico.villa@jbknowledge.com>
2023-11-29 10:44:57 -0300

    fix firebase config file

--- src/firebase.js
+++ src/firebase.js
@@ -3,23 +3,23 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // DEV
-const firebaseConfig = {
-  apiKey: "AIzaSyBkNQRCYF3EwkhSPNJqzFJS36KREvhrPro",
-  authDomain: "mercury-fe382.firebaseapp.com",
-  projectId: "mercury-fe382",
-  storageBucket: "mercury-fe382.appspot.com",
-  messagingSenderId: "225249121302",
-  appId: "1:225249121302:web:505d51ecd7599147881137"
-};
-
 // const firebaseConfig = {
-//   apiKey: "AIzaSyC7b9COfOrS7hi1lLZpzjH8KAu0iF0ZbsI",
-//   authDomain: "mercury-prod-cff3e.firebaseapp.com",
-//   projectId: "mercury-prod-cff3e",
-//   storageBucket: "mercury-prod-cff3e.appspot.com",
-//   messagingSenderId: "166501381472",
-//   appId: "1:166501381472:web:06942376c4fda1119c5a05"
+//   apiKey: "AIzaSyBkNQRCYF3EwkhSPNJqzFJS36KREvhrPro",
+//   authDomain: "mercury-fe382.firebaseapp.com",
+//   projectId: "mercury-fe382",
+//   storageBucket: "mercury-fe382.appspot.com",
+//   messagingSenderId: "225249121302",
+//   appId: "1:225249121302:web:505d51ecd7599147881137"
 // };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7b9COfOrS7hi1lLZpzjH8KAu0iF0ZbsI",
+  authDomain: "mercury-prod-cff3e.firebaseapp.com",
+  projectId: "mercury-prod-cff3e",
+  storageBucket: "mercury-prod-cff3e.appspot.com",
+  messagingSenderId: "166501381472",
+  appId: "1:166501381472:web:06942376c4fda1119c5a05"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
